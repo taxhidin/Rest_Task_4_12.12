78fb664ff3cb4bc0b71179fdae13c27e78255841
taxhidin <taxhidin1@gmail.com>
2021-10-04 00:12:43 +0200

    update

--- src/main/java/com/example/rest_task_2/SummaryService.java
+++ src/main/java/com/example/rest_task_2/SummaryService.java
@@ -1,16 +1,20 @@
-/*
 package com.example.rest_task_2;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class SummaryService {
 
-     User user = new User(1,2,3,4,5,6);
+ //   User user = new User(1,2,3,4,5,6,7,8);
 
 
-    public int result() {
-        int sum_end = user.getFirst() + user.getSecond() + user.getThird() + user.getFourth() + user.getFive() + user.getSix();
+    public int result(int a, int b, int c, int d, int e, int f, int g, int h) {
+        //int sum_end = user.getFirst() + user.getSecond() + user.getThird() + user.getFourth() + user.getFive() + user.getSix();
 
-        return sum_end;
+        int sum =a+b+c+d+e+f+g+h;
+        return sum;
+
+        //return sum_end;
     }
 
 }
-*/
--- src/main/java/com/example/rest_task_2/UserResource.java
+++ src/main/java/com/example/rest_task_2/UserResource.java
@@ -1,11 +1,11 @@
 package com.example.rest_task_2;
 
 
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
@@ -13,29 +13,27 @@ import java.util.HashMap;
 @Path("/hello-world")
 
 public class UserResource {
-    User message = new User(1, 2, 3, 4, 5, 6,7,8);
+   // User user = new User(1,2,3,4,5,6,7,8);
+
+    @Inject
+    SummaryService summaryService;
 
 
+    @Inject
+    Input input_value;
 
     @GET
-    @Path("/input")
+    @Path("/input/{number_1}/{number_2}/{number_3}/{number_4}/{number_5}/{number_6}/{number_7}/{number_8}")
     @Produces(MediaType.APPLICATION_JSON)
 
-        public Response getUsers() {
+        public Response getUsers(@QueryParam("number_1") int first, @QueryParam ("number_2") int second, @QueryParam ("number_3") int third,
+                                 @QueryParam ("number_4") int fourth, @QueryParam ("number_5") int five, @QueryParam ("number_6") int six,
+                                 @QueryParam("number_7") int seven,@QueryParam("number_8") int eight) {
 
 
         try {
-            int[] test = new int[8];
-            test[0] = message.getFirst();
-            test[1] = message.getSecond();
-            test[2] = message.getThird();
-            test[3] = message.getFourth();
-            test[4] = message.getFive();
-            test[5] = message.getSix();
-            test[6] = message.getSeven();
-            test[7] = message.getEight();
 
-            return Response.ok(test).build();
+            return Response.ok(input_value.getInput(first, second, third, fourth, five, six, seven, eight)).build();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -44,24 +42,20 @@ public class UserResource {
 
     }
 
+    @Inject
+
+    Output output;
 
     @GET
-    @Path("/output")
+    @Path("/output/{number_1_a}/{number_2_a}/{number_3_a}/{number_4_a}/{number_5_a}/{number_6_a}/{number_7_a}/{number_8_a}")
     @Produces(MediaType.APPLICATION_JSON)
-        public Response getUsers_1() {
-
+        public Response getUsers_1(@QueryParam("number_1_a") int first_1, @QueryParam ("number_2_a") int second_1, @QueryParam ("number_3_a") int third_1,
+                                   @QueryParam ("number_4_a") int fourth_1, @QueryParam ("number_5_a") int five_1, @QueryParam ("number_6_a") int six_1,
+                                   @QueryParam("number_7_a") int seven_1,@QueryParam("number_8_a") int eight_1) {
 
         try {
 
-            HashMap<String, Integer> map = new HashMap<>();
-
-            int value = message.getFirst() + message.getSecond() + message.getThird() + message.getFourth() + message.getFive() +
-                    message.getSix()+message.getSeven()+message.getEight();
-
-            map.put("result: ", value);
-
-
-            return Response.ok(map).build();
+            return Response.ok(output.getOutput(first_1, second_1, third_1, fourth_1, five_1, six_1, seven_1, eight_1)).build();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -69,10 +63,6 @@ public class UserResource {
         return null;
 
     }
-
-
-
-
 
 
 
